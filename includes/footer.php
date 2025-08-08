7111f763a6cd63f4e1778cd47756495b650d4a31
Sylvebois <thomasghaye@hotmail.com>
2025-08-08 18:48:12 +0200

    Added <p> arournd contact link

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,5 @@
 <footer>
-    <a href="/contact" class="btn">Prendre rendez-vous</a>
+    <p><a href="/contact" class="btn">Prendre rendez-vous</a></p>
     <p>&copy; <?= date('Y') ?> ReikiSan - Tous droits réservés</p>
 </footer>
 </body>
