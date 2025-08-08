c05e20d24bf1f0a24ef489129432e56f7a7ceb36
Sylvebois <thomasghaye@hotmail.com>
2025-08-08 17:19:12 +0200

    Open/Close menu event change

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,7 +1,10 @@
-const menuToggle = document.getElementById('menu-toggle');
+const toggleMenuButton = document.getElementById('toggle-menu');
 const mobileNav = document.getElementById('mobile-nav');
 
-menuToggle.addEventListener('click', e => {
-  console.log('click')
-  mobileNav.classList.toggle('hidden');
+toggleMenuButton.addEventListener('click', e => {
+  mobileNav.classList.toggle('hidden-menu');
+
+  const menuIcons = toggleMenuButton.getElementsByTagName('img');
+  menuIcons[0].classList.toggle('hidden');
+  menuIcons[1].classList.toggle('hidden');
 });
