733240aa896bd5b694f8b75470a2e911ec922f65
Sylvebois <thomasghaye@hotmail.com>
2025-08-08 17:50:45 +0200

    Page and SEO improvement for reiki.php

--- public/reiki.php
+++ public/reiki.php
@@ -1,26 +1,49 @@
 <?php
 $title = "Le Reiki - ReikiSan";
-$description = "Une brève explication de ce qu'est le Reiki et de ce qu'il apporte.";
+$description = "Une brève explication de ce qu'est le Reiki, de ce qu'il apporte comme bienfait pour le corps, l'esprit et les émotions. Une pratique douce et naturelle.";
 require_once __DIR__ . '/../includes/header.php';
 ?>
 
 <main>
-    <h1>Qu'est-ce que le Reiki ?</h1>
-    <p>Le Reiki est une méthode de soins japonaise appartenant à l’approche énergétique, une technique de relaxation méditative qui agit aux niveaux physique, mental, émotionnel, spirituel. 
-        Cette méthode a été fondée au XIX<sup>ème</sup> siècle par <a href="https://fr.wikipedia.org/wiki/Usui_Mikao" target="_blank">Mikao Usui</a>.</p>
-    <p>Le <dfn>Reiki</dfn> signifie : &quot;énergie universelle de vie guidée spirituellement&quot;. Concrètement, ce mot japonais désigne à la fois un concept d’énergie spirituelle (communément appelé <em>énergie vitale universelle</em> ou <em>ki</em>) et
-    l’ensemble des pratiques de guérison et de développement personnelles et spirituelles que permet cette énergie.</p>
-    <p>Le <em>ki</em> est présent dans tout être vivant sous différentes fréquences vibratoires. Pour nous permettre de nous sentir bien, cette énergie devrait circuler, en nous, à une fréquence (taux vibratoire) aussi élevée que possible.</p>
-    <p>Le Reiki apporte d’innombrables bienfaits : 
-    <ul>
-        <li>Augmente le taux vibratoire</li>
-        <li>Accélère le processus de guérison</li>
-        <li>Renforce la vitalité</li>
-        <li>Libère les blocages</li>
-        <li>Favorise la détente</li>
-        <li>...</li>
-    </ul>
-    <p>Le Reiki représente également un outil de développement personnel et spirituel. Il n’implique pas de dogme ou de croyances quelconques et ne remplace pas la médecine classique mais le Reiki lui est complémentaire pour un bien-être naturel et durable.</p>
+        <h1>Qu'est-ce que le Reiki ?</h1>
+    <section>
+      <img src="/public/assets/img/reiki_japonais.svg" alt="Reiki en japonais" width="100px" height="200px" class="img-onpage">
+      <div class="text-container">
+        <h2>Origine et définition</h2>
+        <p>Le Reiki est une méthode de soins japonaise appartenant à l’approche énergétique, une technique de relaxation
+          méditative qui agit aux niveaux physique, mental, émotionnel, spirituel.</p>
+        <p>Le <dfn>Reiki</dfn> signifie : &quot;énergie universelle de vie guidée spirituellement&quot;. Concrètement,
+          ce
+          mot japonais désigne à la fois un concept d’énergie spirituelle (communément appelé <em>énergie vitale
+            universelle</em> ou <em>ki</em>) et
+          l’ensemble des pratiques de guérison et de développement personnelles et spirituelles que permet cette
+          énergie.
+        </p>
+        <p>Cette pratique de soins énergétiques a été fondée au XIX<sup>ème</sup> siècle par <a
+            href="https://fr.wikipedia.org/wiki/Usui_Mikao" target="_blank">Mikao Usui</a>.</p>
+      </div>
+    </section>
+    <section>
+      <div class="text-container">
+        <h2>Les bienfaits du Reiki</h2>
+        <p>Le <em>ki</em> est présent dans tout être vivant sous différentes fréquences vibratoires. Pour nous permettre
+          de nous sentir bien, cette énergie devrait circuler, en nous, à une fréquence (taux vibratoire) aussi élevée
+          que
+          possible.</p>
+        <p>Le Reiki apporte d’innombrables bienfaits :</p>
+        <ul>
+          <li>Augmente le taux vibratoire</li>
+          <li>Accélère le processus de guérison</li>
+          <li>Renforce la vitalité</li>
+          <li>Libère les blocages</li>
+          <li>Favorise la détente</li>
+          <li>...</li>
+        </ul>
+      </div>
+      <img src="/public/assets/img/soins.jpg" alt="Séance de soins Reiki" class="img-onpage">
+    </section>
+      <p style="text-align: justify;">Le Reiki représente également un outil de développement personnel et spirituel. Il n’implique pas de dogme ou
+        de croyances quelconques; <strong>il ne remplace pas la médecine classique mais le Reiki lui est complémentaire</strong> pour un bien-être naturel et durable.</p>
 </main>
 
 <?php require_once __DIR__ . '/../includes/footer.php'; ?>
